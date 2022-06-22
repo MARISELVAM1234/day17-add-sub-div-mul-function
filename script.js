7cf8c8c8321facfb26f385fd3febdf3d980f31c7
selvam <selvamplkm111@gmail.com>
2022-06-22 09:36:34 +0530

    add.folder.

---
+++ script.js
@@ -0,0 +1,54 @@
+// without argument without return value
+//  Addition
+// var a = 30;
+// var b = 40;
+// function addition(){
+//     var x = 30 , y = 40;
+//     var add = (x+y);
+//     document.write("Addition :",add);
+// }
+// addition()
+
+
+
+// without argument with return value
+//  Subtration
+// var a = 50;
+// var b = 20;
+// function subtration() {
+//     var x = 50, y = 20;
+//     var sub = (x - y);
+//     return sub;
+// }
+// result = subtration()
+// document.write("subtration :", result);
+
+
+
+
+// with argument without return value
+//  Multiplication
+// var a = 50;
+// var b = 20;
+// function multiplication(x,y) {
+//     var x = 20, y = 20;
+//     var mul = (x*y);
+//     document.write("multiplication :", mul);
+// }
+// multiplication()
+
+
+
+// with argument with return value
+//  Division
+// var a = 6000;
+// var b = 20;
+// function division(x, y) {
+//     var x = 7000, y = 20;
+//     var div = (x / y);
+//     return div
+
+// }
+// var result =division()
+// document.write("Division :", result);
+  
